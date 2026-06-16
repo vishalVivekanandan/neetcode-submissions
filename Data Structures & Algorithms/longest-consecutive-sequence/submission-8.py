a1ee975e5d0281a928bbe65b6ff970abb3079023
Vishal Vivekanandan <139830326+vishalVivekanandan@users.noreply.github.com>
2026-06-16 18:31:18 -0400

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numset = set(nums)
+        longest = 0
+
+        for num in numset:
+            
+            # keep finding local minima, and compute the longest seq 
+            if (num - 1) not in numset:
+                length = 1
+                while (num + length) in numset:
+                    length += 1
+                longest = max(length, longest)
+        
+        # this must be the lcs
+        return longest
