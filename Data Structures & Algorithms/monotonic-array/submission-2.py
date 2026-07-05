20bd8c0e3031de0a4955c0654727552b23ad52d4
Vishal Vivekanandan <139830326+vishalVivekanandan@users.noreply.github.com>
2026-07-05 15:50:35 -0400

    Add: monotonic-array - submission-2

---
+++ Data Structures & Algorithms/monotonic-array/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        isIncreasing, isDecreasing = True, True
+
+
+
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i-1]:
+                isIncreasing = False
+            if nums[i] > nums[i-1]:
+                isDecreasing = False
+
+        return isIncreasing or isDecreasing
