660e50598f96a81d593dd8f12639ad2b62d4a2ca
Vishal Vivekanandan <139830326+vishalVivekanandan@users.noreply.github.com>
2026-08-10 15:20:38 -0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # hash_s = {}
+        # hash_t = {}
+
+        # for c in s:
+        #     hash_s[c] = 1 + hash_s.get(c, 0)
+
+        # for c in t:
+        #     hash_t[c] = 1 + hash_t.get(c, 0)
+        # return hash_t == hash_s
+        return Counter(s) == Counter(t)
+            
